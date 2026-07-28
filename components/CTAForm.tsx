6c2deda191e61266ab1137278ec1193f43f75e91
byronteo385-eng <byronteo385@gmail.com>
2026-07-28 19:53:13 +0800

    Update CTAForm.tsx

--- components/CTAForm.tsx
+++ components/CTAForm.tsx
@@ -14,7 +14,7 @@
      const data = new FormData(form)
 
      try {
-       const res = await fetch('https://formspree.io/f/mjgnlra', {
+       const res = await fetch('https://formspree.io/f/mjgnlrar', {
          method: 'POST',
          body: data,
          headers: { Accept: 'application/json' },
